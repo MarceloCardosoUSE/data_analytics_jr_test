accf6bb285bb75202e57376a3ad98a6cbf929b2f
Victor Antoniassi <antoniassi.victor@gmail.com>
2024-02-23 15:50:53 -0300

    Pequenos ajustes no comparador_cabecalhos_csv.py

--- solucao_victor_antoniassi/scripts/comparador_cabecalhos_csv.py
+++ solucao_victor_antoniassi/scripts/comparador_cabecalhos_csv.py
@@ -24,7 +24,7 @@ def comparar_cabecalhos(df):
         tuple: Uma tupla contendo:
             - str: "correto" se as colunas estiverem corretas, "incorreto" caso contrário.
             - set: Conjunto de colunas faltantes.
-            - set: Conjunto de colunas em excesso.
+            - set: Conjunto de colunas em excessonomes diferentes.
     """
     cabecalho_correto = "DRE;CODESC;TIPOESC;NOMESC;NOMESCOF;CEU;DIRETORIA;SUBPREF;ENDERECO;NUMERO;BAIRRO;CEP;TEL1;TEL2;FAX;SITUACAO;CODDIST;DISTRITO;SETOR;CODINEP;CODCIE;EH;FX_ETARIA;DT_CRIACAO;ATO_CRIACAO;DOM_CRIACAO;DT_INI_FUNC;DT_INI_CONV;DT_AUTORIZA;DT_EXTINCAO;NOME_ANT;REDE;LATITUDE;LONGITUDE;DATABASE"
     
@@ -47,8 +47,8 @@ def main():
 
     Salva os resultados em um arquivo CSV chamado 'resultado_comparacao_cabecalhos.csv'.
     """
-    diretorio = r'c:\Users\ONNE\Desktop\use_teste_analytics\escolas_csv'
-    df_comparacao = pd.DataFrame(columns=["Arquivo", "Status", "Colunas Faltantes", "Colunas em Excesso"])
+    diretorio = 'datasets/escolas'
+    df_comparacao = pd.DataFrame(columns=["Arquivo", "Status", "Colunas faltantes", "Colunas em excesso-nomes diferentes"])
 
     for arquivo in os.listdir(diretorio):
         if arquivo.endswith(".csv"):
@@ -62,11 +62,11 @@ def main():
             df_comparacao = pd.concat([df_comparacao, pd.DataFrame({
                 "Arquivo": [arquivo],
                 "Status": [status],
-                "Colunas Faltantes": [" | ".join(colunas_faltantes_lista)],
-                "Colunas em Excesso": [" | ".join(colunas_em_excesso)]
+                "Colunas faltantes": [" | ".join(colunas_faltantes_lista)],
+                "Colunas em excesso-nomes diferentes": [" | ".join(colunas_em_excesso)]
             })], ignore_index=True)
 
-    df_comparacao.to_csv('resultado_comparacao_cabecalhos.csv', sep=";", index=False)
+    df_comparacao.to_csv('datasets/escolas/resultado_comparacao_cabecalhos_escolas.csv', sep=";", index=False)
 
 if __name__ == "__main__":
     main()
