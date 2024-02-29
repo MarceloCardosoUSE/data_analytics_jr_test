ad84b4eb92b33c13285b53df794637de5b188afa
Victor Antoniassi <antoniassi.victor@gmail.com>
2024-02-29 17:35:58 -0300

    Refatoração e criação de novo script
    Refatoração no comparador_cabecalhos_csv.py e começo do desenvolvimento do datasets_to_sqlite.py

--- solucao_victor_antoniassi/scripts/comparador_cabecalhos_csv/comparador_cabecalhos_csv.py
+++ solucao_victor_antoniassi/scripts/comparador_cabecalhos_csv/comparador_cabecalhos_csv.py
@@ -1,75 +1,57 @@
 import os
 import pandas as pd
 
-def carregar_csv(caminho_arquivo):
+# Definindo a função comparador_cabecalhos_csv
+def comparador_cabecalhos_csv(diretorio, cabecalho_correto, delimitador, encoding):
     """
-    Carrega um arquivo CSV e retorna um DataFrame do Pandas.
+    Esta função processa arquivos CSV em um diretório especificado, verifica se o cabeçalho de cada arquivo corresponde ao cabeçalho correto fornecido e gera um relatório de comparação.
 
-    Args:
-        caminho_arquivo (str): O caminho para o arquivo CSV.
+    Parâmetros:
+    diretorio (str): O caminho para o diretório que contém os arquivos CSV.
+    cabecalho_correto (str): Uma string delimitada por ponto e vírgula do cabeçalho correto.
+    delimitador (str): O delimitador usado nos arquivos CSV.
+    encoding (str): A codificação usada nos arquivos CSV.
 
-    Returns:
-        pandas.DataFrame: O DataFrame contendo os dados do arquivo CSV.
+    Retorna:
+    None. A função salva um arquivo CSV com o relatório de comparação no diretório especificado.
     """
-    return pd.read_csv(caminho_arquivo, delimiter=";", encoding='latin-1')
-
-def comparar_cabecalhos(df):
-    """
-    Compara as colunas de um DataFrame com um cabeçalho correto predefinido.
-
-    Args:
-        df (pandas.DataFrame): O DataFrame a ser comparado.
-
-    Returns:
-        tuple: Uma tupla contendo:
-            - str: "correto" se as colunas estiverem corretas, "incorreto" caso contrário.
-            - set: Conjunto de colunas faltantes.
-            - set: Conjunto de colunas em excessonomes diferentes.
-    """
-
-    cabecalho_correto = "DRE;CODESC;TIPOESC;NOMESC;DISTRITO;SETOR;ANO;REDE;MODAL;DESCSERIE;PERIODO;TURNO;DESCTURNO;SEXO;IDADE;NEE;RACA;QTDE;DATABASE"
-    
-    # Converter nomes das colunas para maiúsculas
-    df.columns = [col.upper() for col in df.columns]
-    
-    colunas_faltantes = set(cabecalho_correto.split(";")) - set(df.columns)
-    colunas_em_excesso = set(df.columns) - set(cabecalho_correto.split(";"))
-    
-    if not colunas_faltantes and not colunas_em_excesso:
-        return "correto", colunas_faltantes, colunas_em_excesso
-    else:
-        return "incorreto", colunas_faltantes, colunas_em_excesso
-
-def main():
-    """
-    Função principal que compara cabeçalhos de arquivos CSV em um diretório.
-
-    Cria um DataFrame com os resultados das comparações.
-
-    Salva os resultados em um arquivo CSV chamado 'resultado_comparacao_cabecalhos.csv'.
-    """
-    diretorio = 'datasets_originais\\educandos'
+    # Inicializando um DataFrame para comparação
     df_comparacao = pd.DataFrame(columns=["Arquivo", "Status", "Colunas faltantes", "Colunas em excesso-nomes diferentes"])
-
-    #colocar o caminho para o diretório de datasets originais de escolas ou educandos
-    for arquivo in os.listdir('datasets_originais\\educandos'):
+    
+    # Iterando sobre todos os arquivos no diretório especificado
+    for arquivo in os.listdir(diretorio):
+        # Verificando se o arquivo é um arquivo .csv
         if arquivo.endswith(".csv"):
+            # Construindo o caminho completo para o arquivo
             caminho_arquivo = os.path.join(diretorio, arquivo)
-            df = carregar_csv(caminho_arquivo)
-            status, colunas_faltantes, colunas_em_excesso = comparar_cabecalhos(df)
-
-            # Converta o conjunto colunas_faltantes para uma lista
+            
+            # Lendo o arquivo .csv para um DataFrame
+            df = pd.read_csv(caminho_arquivo, delimiter=delimitador, encoding=encoding)
+            
+            # Convertendo todos os nomes das colunas para maiúsculas
+            df.columns = [col.upper() for col in df.columns]
+            
+            # Identificando as colunas faltantes e em excesso
+            colunas_faltantes = set(cabecalho_correto.split(";")) - set(df.columns)
+            colunas_em_excesso = set(df.columns) - set(cabecalho_correto.split(";"))
+            
+            # Determinando o status do arquivo
+            status = "correto" if not colunas_faltantes and not colunas_em_excesso else "incorreto"
+            
+            # Convertendo os conjuntos de colunas para listas
             colunas_faltantes_lista = list(colunas_faltantes)
-
+            
+            # Adicionando os resultados ao DataFrame de comparação
             df_comparacao = pd.concat([df_comparacao, pd.DataFrame({
                 "Arquivo": [arquivo],
                 "Status": [status],
                 "Colunas faltantes": [" | ".join(colunas_faltantes_lista)],
                 "Colunas em excesso-nomes diferentes": [" | ".join(colunas_em_excesso)]
             })], ignore_index=True)
+    
+    # Salvando o DataFrame de comparação como um arquivo .csv
+    df_comparacao.to_csv(os.path.join(diretorio, 'resultado_comparacao_cabecalhos_escolas.csv'), sep=";", index=False)
 
-    #colocar o caminho para o diretorio de salvamento do resultado e o nome do arquivo de resultado
-    df_comparacao.to_csv('datasets_originais\\educandos\\resultado_comparacao_cabecalhos_educandos.csv', sep=";", index=False)
-
-if __name__ == "__main__":
-    main()
+# Uso da função:
+cabecalho_correto = "DRE;CODESC;TIPOESC;NOMESC;NOMESCOF;CEU;DIRETORIA;SUBPREF;ENDERECO;NUMERO;BAIRRO;CEP;TEL1;TEL2;FAX;SITUACAO;CODDIST;DISTRITO;SETOR;CODINEP;CODCIE;EH;FX_ETARIA;DT_CRIACAO;ATO_CRIACAO;DOM_CRIACAO;DT_INI_FUNC;DT_INI_CONV;DT_AUTORIZA;DT_EXTINCAO;NOME_ANT;REDE;LATITUDE;LONGITUDE;DATABASE"
+comparador_cabecalhos_csv('datasets_originais\\escolas', cabecalho_correto, ";", 'latin-1')
